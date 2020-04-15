0fa00a56cbc6f6e15f80a0c4071d97cc7249b29f
pepper <pepper_fate@163.com>
2020-04-15 20:42:07 +0800

    Q144 by link method

--- src/main/java/stack/Question144.java
+++ src/main/java/stack/Question144.java
@@ -18,10 +18,34 @@ public class Question144 {
     }
 
     public List<Integer> preorderTraversal(TreeNode root) {
-        return preorderTraversal_stack(root);
+        TreeNode cur = root;
+        LinkedList<Integer> res = new LinkedList<>();
+        while (cur != null) {
+            if (cur.left == null) {
+                res.add(cur.val);
+                cur = cur.right;
+            } else {
+                TreeNode inorderTraversalPre = findInorderTraversalPre(cur);
+                if (inorderTraversalPre.right == null) {
+                    res.add(cur.val);
+                    inorderTraversalPre.right = cur; // link
+                    cur = cur.left;
+                } else {
+                    inorderTraversalPre.right = null; // unlink
+                    cur = cur.right;
+                }
+            }
+        }
+        return res;
     }
 
-
+    private TreeNode findInorderTraversalPre(TreeNode cur) {
+        TreeNode pre = cur.left;
+        while (pre.right != null && pre.right != cur) {
+            pre = pre.right;
+        }
+        return pre;
+    }
 
 
     public List<Integer> preorderTraversal_stack(TreeNode node) {
@@ -30,7 +54,7 @@ public class Question144 {
 
         TreeNode cur = node;
         while (cur != null || !stack.isEmpty()) {
-            if(cur == null){
+            if (cur == null) {
                 cur = stack.pop();
             }
             res.add(cur.val);
